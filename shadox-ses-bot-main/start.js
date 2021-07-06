c43866744e34334624caa0194012635444f130bc
ShadoxXD <84858225+ShadoxXD@users.noreply.github.com>
2021-07-06 20:57:03 +0300

    Update start.js

--- shadox-ses-bot-main/start.js
+++ shadox-ses-bot-main/start.js
@@ -1,5 +1,5 @@
 console.log('GİRİŞ SAĞLANDI! [ Developed By Shadox ]')
-require("./sesbotu.js");
+require("./31komiktir.js");
 console.log(' Modüller Aktif! [ Developed By Shadox ]')
 console.log('Belirlenen Ses Dosyası yüklendi! [ Developed By Shadox ]')
 console.log('Başarıyla Oynatılıyor! [ Developed By Shadox ]')
